69654a88b5ee8f12b96c7c20088214bfc8a502ac
Erik Ivanov <erik.ivanov.n@gmail.com>
2022-04-26 00:24:40 -0500

    functions

---
+++ src/clean_code/03-functions.ts
@@ -0,0 +1,36 @@
+(() => {
+  function getMovieById(movieId: string) {
+    console.log({ movieId });
+  }
+
+  function getMovieCastById(movieId: string) {
+    console.log({ movieId });
+  }
+
+  function getActorBiographyById(ActorId: string) {
+    console.log({ ActorId });
+  }
+
+  interface Movie {
+    cast: string[];
+    description: string;
+    rating: number;
+    title: string;
+  }
+
+  function createMovie({ title, description, rating, cast }: Movie) {
+    console.log({ title, description, rating, cast });
+  }
+
+  function checkFullName(fullName: string): boolean {
+    console.log({ fullName });
+    return true;
+  }
+
+  function createActor(fullName: string, birthdate: Date): boolean {
+    if (!checkFullName(fullName)) return false;
+
+    console.log('Crear actor', birthdate);
+    return true;
+  }
+})();
